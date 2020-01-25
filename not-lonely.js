7bfc5f05ac84aa6523f5ba76f73162c8a5df915e
beepboop271 <53926222+beepboop271@users.noreply.github.com>
2020-01-24 22:31:07 -0500

    changed to dotenv and master branch of discordjs
    -google cloud and discord use env vars
    -no ffmpeg re-encoding because of updated discord.js
    -package.json and license exists

--- not-lonely.js
+++ not-lonely.js
@@ -1,13 +1,14 @@
-const fs = require('fs');
-const process = require('child_process');
-const util = require('util');
+const fs = require("fs");
+const util = require("util");
 
-const auth = require('./auth.json');
-const Discord = require('discord.js');
-const Tts = require('@google-cloud/text-to-speech');
+require("dotenv-safe").config();
+const Discord = require("discord.js");
+const Tts = require("@google-cloud/text-to-speech");
 
 const discordClient = new Discord.Client();
-discordClient.on('ready', () => {console.log('logged in');});
+discordClient.on("ready", () => {
+  console.log("logged in");
+});
 
 // chr(x) from python, idk
 // i just didn't want to type so much
@@ -15,124 +16,127 @@ let chr = String.fromCharCode;
 
 let ttsConfig = {
   isUsing: false,
-  channel: '',
-  speaker: '',
+  channel: "",
+  speaker: "",
   conn: null,
-  voice: 'en-US-Wavenet-B',
-  lang: 'en-US'
+  voice: "en-US-Wavenet-B",
+  lang: "en-US"
 };
 let fileName = 0;
 
 async function createTTSAudio(text, callback) {
   // google cloud stuff
-  let ttsClient = new Tts.TextToSpeechClient(credentials=auth.keyPath);
+  let ttsClient = new Tts.TextToSpeechClient();
   let request = {
-    input: {text:text},
-    voice: {languageCode: ttsConfig.lang, name: ttsConfig.voice},
-    audioConfig: {audioEncoding: 'MP3'}
+    input: { text: text },
+    voice: { languageCode: ttsConfig.lang, name: ttsConfig.voice },
+    audioConfig: { audioEncoding: "MP3" }
   };
   let [response] = await ttsClient.synthesizeSpeech(request);
 
   // write the file, add silence
-  fileName = (fileName+1)%5;
-  let path = 'audio/'+chr(48+fileName)+'.mp3';
-  fs.writeFile(path, response.audioContent, 'binary', err => {
-    if(err) console.log(err);
-    // apparently discord.js has an issue with cutting off the end of audio
-    // files and apparently it is fixed in the development version so
-    // just add some silence thats good enough for me
-    process.exec(util.format('ffmpeg -i %s.mp3 -af "apad=pad_dur=1" -y -hide_banner -loglevel panic %s.mp3',
-                             chr(48+fileName), chr(97+fileName)),
-                 {cwd:auth.projectPath+'audio', windowsHide:true},
-                 (err, stdout, stderr) => {
-                   if(err) console.log(err);
-                   callback(util.format('audio/%s.mp3', chr(97+fileName)));
-                 }
-    );
+  fileName = (fileName + 1) % 5;
+  let path = "audio/" + chr(48 + fileName) + ".mp3";
+  fs.writeFile(path, response.audioContent, "binary", err => {
+    if (err) console.log(err);
+    callback(util.format("audio/%s.mp3", chr(48 + fileName)));
+
+    // discord.js stable version has bug where short files are
+    // cut off, issue is fixed in master so this sketchy
+    // ffmpeg re-encoding is no longer needed
+    // ChildProcess.exec(
+    //     util.format('ffmpeg -i %s.mp3 -af "apad=pad_dur=1" -y -hide_banner -loglevel panic %s.mp3',
+    //                 chr(48+fileName), chr(97+fileName)),
+    //     {cwd:auth.projectPath+'audio', windowsHide:true},
+    //     (err, stdout, stderr) => {
+    //       if(err) console.log(err);
+    //       callback(util.format('audio/%s.mp3', chr(97+fileName)));
+    //     }
+    // );
   });
 }
 
-discordClient.on('message', msg => {
-  if(!msg.guild) return;
+discordClient.on("message", msg => {
+  if (!msg.guild) return;
 
   // if we need to speak this message
-  if(ttsConfig.isUsing && msg.channel.id == ttsConfig.channel && msg.author.id == ttsConfig.speaker) {
-    if(msg.content.substring(0, 1) == '*') {
-      args = msg.content.substring(1).split(' ');
+  if (ttsConfig.isUsing
+        && msg.channel.id == ttsConfig.channel
+        && msg.author.id == ttsConfig.speaker) {
+    if (msg.content.substring(0, 1) == "*") {
+      args = msg.content.substring(1).split(" ");
 
-      if(args[0] == 'help') {
-        msg.reply('`*leave` to be lonely again, `*set-voice lang voice` to change voice (`*set-voice en-US B`)')
+      if (args[0] == "help") {
+        msg.reply("`*leave` to be lonely again, `*set-voice lang voice` to change voice (`*set-voice en-US B`)")
           .catch(console.log);
-      } else if(args[0] == 'leave') {
+      } else if (args[0] == "leave") {
         ttsConfig.isUsing = false;
-        ttsConfig.channel = '';
-        ttsConfig.speaker = '';
+        ttsConfig.channel = "";
+        ttsConfig.speaker = "";
         ttsConfig.conn.disconnect();
         ttsConfig.conn = null;
-      } else if(args[0] == 'set-voice') {
+      } else if (args[0] == "set-voice") {
         ttsConfig.lang = args[1];
-        ttsConfig.voice = args[1]+'-Wavenet-'+args[2];
+        ttsConfig.voice = args[1] + "-Wavenet-" + args[2];
         console.log(ttsConfig.voice);
         console.log(ttsConfig.lang);
-      } else if(args[0] == 'mine-all-day') {
+      } else if (args[0] == "mine-all-day") {
         // haha yes
-        const dispatcher = ttsConfig.conn.playFile('audio/Mine All Day (Minecraft Music Video).mp3');
-        dispatcher.on('end', end => {
-          console.log('hi');
-        });
+        ttsConfig.conn.play("audio/Mine All Day (Minecraft Music Video).mp3");
       }
     } else {
       createTTSAudio(msg.content, path => {
-        console.log(msg.content);
-        const dispatcher = ttsConfig.conn.playFile(path);
-        dispatcher.on('end', end => {
-          console.log('hi');
-        });
+        ttsConfig.conn.play(path);
       });
     }
-  } else if(msg.content.substring(0, 1) == '*') {
-    let args = msg.content.substring(1).split(' ');
+  } else if (msg.content.substring(0, 1) == "*") {
+    let args = msg.content.substring(1).split(" ");
 
-    switch(args[0]) {
-      case 'ping':
+    switch (args[0]) {
+      case "ping":
         let startTime = Discord.SnowflakeUtil.deconstruct(msg.id).timestamp;
-        msg.channel.send('pong uwu')
+        msg.channel
+          .send("pong uwu")
           .then(response => {
-            response.edit('pong uwu: '
-                          + (Discord.SnowflakeUtil.deconstruct(response.id).timestamp-startTime).toString()
-                          + 'ms');
+            response.edit(
+              "pong uwu: "
+              + (Discord.SnowflakeUtil.deconstruct(response.id).timestamp-startTime).toString()
+              + "ms"
+            );
           })
           .catch(console.log);
         break;
-
-      case 'im-lonely':
-        if(msg.member.voiceChannel) {
-          if(!ttsConfig.isUsing) {
-            msg.member.voiceChannel.join()
-            .then(voiceConn => {
-              ttsConfig.isUsing = true;
-              ttsConfig.channel = msg.channel.id;
-              ttsConfig.speaker = msg.author.id;
-              ttsConfig.conn = voiceConn;
-              msg.channel.send('voice channel joined');
-            })
-            .catch(console.log);
+      case "im-lonely":
+        if (msg.member.voice.channel) {
+          if (!ttsConfig.isUsing) {
+            msg.member.voice.channel
+              .join()
+              .then(voiceConn => {
+                ttsConfig.isUsing = true;
+                ttsConfig.channel = msg.channel.id;
+                ttsConfig.speaker = msg.author.id;
+                ttsConfig.conn = voiceConn;
+                msg.channel.send("voice channel joined");
+              })
+              .catch(console.log);
           } else {
-            msg.reply('tts is being used by someone else and my node.js skills are nonexistent')
+            msg.reply("tts is being used by someone else and my node.js skills are nonexistent")
               .catch(console.log);
           }
         } else {
-          msg.reply('you are not in a voice channel')
+          msg.reply("you are not in a voice channel")
             .catch(console.log);
         }
         break;
-
-      case 'help':
-        msg.reply('`*ping` to ping, `*im-lonely` to be less lonely')
+      case "help":
+        msg.reply("`*ping` to ping, `*im-lonely` to be less lonely")
           .catch(console.log);
         break;
+      default:
+        msg.reply("unknown command "+args[0])
+          .catch(console.log);
     }
   }
 });
 
-discordClient.login(auth.discordToken);
+discordClient.login(process.env.DISCORD_BOT_TOKEN);
